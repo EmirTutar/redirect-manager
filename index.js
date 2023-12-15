b40ebc7256eff1d59c7b552503d6e7441b3b95e1
Emircan Tutar <emircan.tutar@hs-weingarten.de>
2023-12-15 16:39:28 +0100

    env

--- index.js
+++ index.js
@@ -1,87 +1,91 @@
-// index.js
+
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
-
 app.use(express.json());
 
-// Lese die Slugs aus der JSON-Datei beim Start des Servers
-let entries = loadSlugs();
+const dataPath = './redirects.json';
 
-// Middleware für die Authentifizierung
 const authenticate = (req, res, next) => {
-  const token = req.headers.authorization;
+    const token = req.headers.authorization;
+  
+    if (token === 'IHR_BEARER_TOKEN') {
+      next();
+    } else {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+  };
 
-  // Hier sollte Ihre Logik zur Überprüfung des Tokens stehen.
-  // Das folgende ist ein einfaches Beispiel, bitte durch sicherere Methoden ersetzen.
-  if (token === 'IhrBearerToken') {
-    next(); // Fortsetzen mit der nächsten Middleware oder Route
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
+const readData = () => {
+  try {
+    return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  } catch (err) {
+    console.error(err);
+    return {};
   }
 };
 
-// GET /:slug
-app.get('/:slug', (req, res) => {
-  const slug = req.params.slug;
+// Schreibt die Daten in die JSON-Datei
+const writeData = (data) => {
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+};
 
-  // Überprüfen, ob der Slug in der Datenstruktur vorhanden ist
-  if (entries.hasOwnProperty(slug)) {
-    res.redirect(entries[slug].url);
-  } else {
-    res.status(404).json({ message: 'Slug not found' });
-  }
-});
-
-// GET /entries (Mit Authentication Middleware)
-app.get('/entries', authenticate, (req, res) => {
-  res.json(entries);
-});
-
-// DELETE /entry/:slug
-app.delete('/entry/:slug', authenticate, (req, res) => {
-  const slugToDelete = req.params.slug;
-  if (entries.hasOwnProperty(slugToDelete)) {
-    delete entries[slugToDelete];
-    saveSlugs(entries); // Speichere die Änderungen in der JSON-Datei
-    res.json({ message: 'Entry deleted successfully' });
-  } else {
-    res.status(404).json({ message: 'Slug not found' });
-  }
-});
-
-// POST /entry
+// Hinzufügen eines neuen Redirect-Eintrags
 app.post('/entry', authenticate, (req, res) => {
   const { slug, url } = req.body;
 
-  // Überprüfen, ob der Slug bereits existiert
-  if (entries.hasOwnProperty(slug)) {
-    res.status(409).json({ message: 'Slug already exists' });
+  if (!slug || !url) {
+    return res.status(400).send('Slug und URL sind erforderlich.');
+  }
+
+  const data = readData();
+  data[slug] = url;
+  writeData(data);
+
+  res.status(201).send(`Redirect für ${slug} hinzugefügt.`);
+});
+
+app.delete('/entry/:slug', authenticate, (req, res) => {
+    const slug = req.params.slug;
+    const data = loadRedirectData();
+    
+    if (data[slug]) {
+      delete data[slug];
+      saveRedirectData(data);
+      res.json({ message: 'Entry deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Not Found' });
+    }
+  });
+
+// Umleitung basierend auf dem Slug
+app.get('/:slug', (req, res) => {
+  const { slug } = req.params;
+  const data = readData();
+
+  if (data[slug]) {
+    return res.redirect(data[slug]);
   } else {
-    // Speichern Sie den Eintrag
-    entries[slug] = { slug, url };
-    saveSlugs(entries); // Speichere die Änderungen in der JSON-Datei
-    res.json({ message: 'Entry added successfully', slug, url });
+    return res.status(404).send('Slug nicht gefunden.');
   }
 });
 
-function loadSlugs() {
-  try {
-    const slugsData = fs.readFileSync('slugs.json', 'utf8');
-    return JSON.parse(slugsData);
-  } catch (error) {
-    // Falls die Datei nicht vorhanden ist oder ein Fehler beim Lesen auftritt, starte mit einer leeren Datenstruktur
-    return {};
+app.delete('/entry/:slug', authenticate, (req, res) => {
+  const slug = req.params.slug;
+  const data = loadRedirectData();
+  
+  if (data[slug]) {
+    delete data[slug];
+    saveRedirectData(data);
+    res.json({ message: 'Entry deleted successfully' });
+  } else {
+    res.status(404).json({ error: 'Not Found' });
   }
-}
+});
 
-function saveSlugs(entries) {
-  const slugsData = JSON.stringify(entries, null, 2);
-  fs.writeFileSync('slugs.json', slugsData);
-}
-
-app.listen(port, () => {
-  console.log(`Server läuft auf http://localhost:${port}`);
+// Starten des Servers
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
 });
